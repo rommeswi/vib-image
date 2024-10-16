7452b261ef9ea028414ba16a68bfcfb7a27e5dc3
rommeswi <rommeswi@protonmail.com>
2024-10-17 01:34:56 +0900

    Completes 1_shell configuration

--- includes.container/shellrc/install-shell.sh
+++ includes.container/shellrc/install-shell.sh
@@ -3,5 +3,5 @@ mkdir -p /tmp/shell/
 cd /tmp/shell/
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/usr/local/bin
 curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json 
-cp /tmp/shell/1_shell.omp.json /etc/skel/.config/oh-my-posh/1_shell.omp.json
+cp /tmp/shell/1_shell.omp.json /etc/skel/.config/oh-my-posh/config.json
 cp /shellrc/.zshrc /etc/skel/.zshrc
