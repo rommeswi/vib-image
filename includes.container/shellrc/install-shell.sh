57952f739b79fc1fbd909809967f3e90b4ed9056
rommeswi <rommeswi@protonmail.com>
2024-10-17 01:31:15 +0900

    Change oh my posh theme to 1_shell

--- includes.container/shellrc/install-shell.sh
+++ includes.container/shellrc/install-shell.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+mkdir -p /tmp/shell/
+cd /tmp/shell/
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/usr/local/bin
+curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json 
+cp /tmp/shell/1_shell.omp.json /etc/skel/.config/oh-my-posh/1_shell.omp.json
 cp /shellrc/.zshrc /etc/skel/.zshrc
