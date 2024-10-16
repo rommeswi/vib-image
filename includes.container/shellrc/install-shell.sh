ef8c89b38661aaacf5cabe1a6c80e6ccbd1dd7bb
rommeswi <rommeswi@protonmail.com>
2024-10-16 22:52:50 +0900

    change oh my posh location

--- includes.container/shellrc/install-shell.sh
+++ includes.container/shellrc/install-shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/usr/local/bin
 cp /shellrc/.zshrc /etc/skel/.zshrc
