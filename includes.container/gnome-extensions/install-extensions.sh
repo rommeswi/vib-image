0447dc9b55758b3771e87342c5c8e7c4bcbde271
rommeswi <rommeswi@protonmail.com>
2024-10-18 02:53:36 +0900

    adds ddterm, alph grid, burn my windows

--- includes.container/gnome-extensions/install-extensions.sh
+++ includes.container/gnome-extensions/install-extensions.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 # Shell script to download and install gnome extensions
-# TOTP
-# window chooser in overview
+
 # burn my windows
-# system resources
+git clone https://github.com/Schneegans/Burn-My-Windows.git /tmp/Burn-My-Windows
+cd /tmp/Burn-My-Windows
+make install
+
 # alphabetical app grid
-# user themes
-# removable drive menu
-# system monitor
-# pomodoro?
+# requires gettext (deb)
+git clone https://github.com/stuarthayhurst/alphabetical-grid-extension.git /tmp/alphabetical-grid-extension
+cd /tmp/alphabetical-grid-extension
+make build
+make install
+
+# ddterm (maybe downloading the zip is faster)
+# requires meson, gtk-builder-tool, gtk4-builder-tool, xsltproc, msgcmp, msgmerge, xgettext, zip
+git clone https://github.com/ddterm/gnome-shell-extension-ddterm.git /tmp/gnome-shell-extension-ddterm
+cd /tmp/gnome-shell-extension-ddterm
+meson setup build-dir
+ninja -C build-dir pack
+gnome-extensions install -f /tmp/gnome-shell-extension-ddterm/build-dir/ddterm@amezin.github.com.shell-extension.zip
+
 # run or raise?
-# day starts on monday
-# clipboard manager?
+# TOTP
+# user themes
+# week starts on monday
+# windowNavigator
 
 # gtile
 # requires npm, git
@@ -21,7 +35,3 @@ npm ci
 npm run build:dist
 npm run install:extension
 
-# Burn my windows
-# git clone https://github.com/Schneegans/Burn-My-Windows.git /tmp/Burn-My-Windows
-# cd /tmp/Burn-My-Windows
-# make install
