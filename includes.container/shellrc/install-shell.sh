ec5032cf69598d38b98a66cb8d9afe65317c5f4a
rommeswi <rommeswi@protonmail.com>
2024-10-17 15:31:04 +0900

    Fix oh my posh installation

--- includes.container/shellrc/install-shell.sh
+++ includes.container/shellrc/install-shell.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Install oh-my-posh
 mkdir -p /tmp/shell/
 cd /tmp/shell/
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/usr/local/bin
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/local/bin
 curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json 
+
+# Copy oh-my-posh configuration
+mkdir -p /etc/skel/.config/oh-my-posh/
 cp /tmp/shell/1_shell.omp.json /etc/skel/.config/oh-my-posh/config.json
 
+# Copy zshrc
 cp /shellrc/.zshrc /etc/skel/.zshrc
 
+# Copy kitty config
 mkdir -p /etc/skel/.config/kitty/
 cp /shellrc/kitty.conf /etc/skel/.config/kitty/kitty.conf
