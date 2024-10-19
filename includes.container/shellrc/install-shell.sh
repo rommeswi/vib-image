e99e4fc98aee451ad6364e9491df3ce737938f56
rommeswi <rommeswi@protonmail.com>
2024-10-20 00:27:35 +0900

    Changes structure of zsh configuration

--- includes.container/shellrc/install-shell.sh
+++ includes.container/shellrc/install-shell.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+# Copy zshrc
+cp /shellrc/.zshenv /etc/skel/.zshenv
+cp /shellrc/.config/zsh /etc/skel/.config/zsh
+
 # Install oh-my-posh
 mkdir -p /tmp/shell/
 cd /tmp/shell/
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/local/bin
-curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json -o 1_shell.omp.json
 
 # Copy oh-my-posh configuration
-mkdir -p /etc/skel/.config/oh-my-posh/themes
-cp /root/.cache/oh-my-posh/themes /etc/skel/.config/oh-my-posh/themes
-cp /tmp/shell/1_shell.omp.json /etc/skel/.config/oh-my-posh/config.json
+mkdir -p /etc/skel/.config/zsh/oh-my-posh/themes
+cp /root/.cache/oh-my-posh/themes /etc/skel/.config/zsh/oh-my-posh/themes
+cp /etc/skel/.config/zsh/oh-my-posh/themes/1_shell.omp.json /etc/skel/.config/zsh/oh-my-posh/config.json
 
 # Install Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ".config/zsh/.zprezto"
-
-# Copy zshrc
-cp /shellrc/.zshrc /etc/skel/.zshrc
+git clone --recursive https://github.com/sorin-ionescu/prezto.git /etc/skel/.config/zsh/zprezto
 
 # Copy kitty config
 mkdir -p /etc/skel/.config/kitty/
