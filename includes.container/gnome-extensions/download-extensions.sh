cf8d642da917259b7828b91179ff436f8b148dde
rommeswi <rommeswi@protonmail.com>
2024-11-01 18:46:26 +0900

    Update download-extensions.sh

--- includes.container/gnome-extensions/download-extensions.sh
+++ includes.container/gnome-extensions/download-extensions.sh
@@ -57,7 +57,7 @@ for i in $(seq 0 $(echo "$repos" | yq 'length-1')); do
         mkdir -p /usr/share/gnome-shell/extensions/tmp
         unzip "$ASSET_NAME" -d /usr/share/gnome-shell/extensions/tmp
         ASSET_UUID=$(jq -r '.uuid' /usr/share/gnome-shell/extensions/tmp/metadata.json)
-        mv -r tmp "/usr/share/gnome-shell/extensions/$ASSET_UUID"
+        mv tmp "/usr/share/gnome-shell/extensions/$ASSET_UUID"
         rm "$ASSET_NAME"
     else
         echo "Error: Failed to download $ASSET_NAME from $REPO."
