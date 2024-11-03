4d60747f98195e522937558090f1e56fd5c64582
rommeswi <rommeswi@protonmail.com>
2024-11-03 14:34:54 +0900

    Update python.lua

--- includes.container/neovim/.config/nvim/lua/plugins/python.lua
+++ includes.container/neovim/.config/nvim/lua/plugins/python.lua
@@ -40,7 +40,7 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "blue",
+        "black",
         "debugpy",
         "pyre",
         "python-lsp-server",
