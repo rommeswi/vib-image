e6e6e6a2c7ff9239cab33154b3b2d310f70aac35
rommeswi <rommeswi@protonmail.com>
2024-11-03 19:33:44 +0900

    Create ranger.lua

---
+++ includes.container/neovim/.config/nvim/lua/plugins/ranger.lua
@@ -0,0 +1,12 @@
+{
+  "kelly-lin/ranger.nvim",
+  config = function()
+    require("ranger-nvim").setup({ replace_netrw = true })
+    vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+      noremap = true,
+      callback = function()
+        require("ranger-nvim").open(true)
+      end,
+    })
+  end,
+}
