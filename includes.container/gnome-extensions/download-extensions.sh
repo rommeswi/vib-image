76f89240b0b9296c90b3dac5c2b28c0107b576b5
Hendrik ROMMESWINKEL <hendrik@localhost>
2024-11-07 11:49:40 +0900

    Fixes extension installation

--- includes.container/gnome-extensions/download-extensions.sh
+++ includes.container/gnome-extensions/download-extensions.sh
@@ -5,16 +5,16 @@ YAML_FILE="/gnome-extensions/extensions.yml"
 
 # Check if file exists.
 if [ -f "$YAML_FILE" ]; then
-        echo "Processing Gnome Extensions in $YAML_FILE."
+  echo "Processing Gnome Extensions in $YAML_FILE."
 else
-    echo "Error: Failed to find $YAML_FILE."
-    exit 1
+  echo "Error: Failed to find $YAML_FILE."
+  exit 1
 fi
 
 # Check if yq is installed
-if ! command -v yq &> /dev/null; then
-    echo "Error: yq is not installed. Please install yq to parse the YAML file."
-    exit 1
+if ! command -v yq &>/dev/null; then
+  echo "Error: yq is not installed. Please install yq to parse the YAML file."
+  exit 1
 fi
 
 # Parse the YAML file and loop through each repository and asset
@@ -23,43 +23,50 @@ repos=$(yq '.repositories' $YAML_FILE)
 mkdir -p /usr/share/gnome-shell/extensions
 
 for i in $(seq 0 $(echo "$repos" | yq 'length-1')); do
-    REPO=$(echo "$repos" | yq -r ".[$i].repo")
-    ASSET_NAME=$(echo "$repos" | yq -r ".[$i].asset")
-    
-    # Make extensions directory
-    
-    echo "Processing $REPO for asset $ASSET_NAME..."
+  REPO=$(echo "$repos" | yq -r ".[$i].repo")
+  ASSET_NAME=$(echo "$repos" | yq -r ".[$i].asset")
 
-    # Encode the @ symbol in the asset name to match URL encoding
-    ENCODED_ASSET_NAME=$(echo "$ASSET_NAME" | sed 's/@/%40/g')
+  # Make extensions directory
 
-    echo "Converted asset name to $ENCODED_ASSET_NAME."
+  echo "Processing $REPO for asset $ASSET_NAME..."
 
-    # Fetch the latest release from the GitHub API
-    API_URL="https://api.github.com/repos/$REPO/releases/latest"
-    echo "Fetching asset url from $API_URL..."
-    
-    ASSET_URL=$(curl -s $API_URL | grep "browser_download_url" | grep "$ENCODED_ASSET_NAME" | grep -v ".asc" | cut -d '"' -f 4)
-    echo "Obtained asset url $ASSET_URL."
+  # Encode the @ symbol in the asset name to match URL encoding
+  ENCODED_ASSET_NAME=$(echo "$ASSET_NAME" | sed 's/@/%40/g')
 
-    # Check if we got the asset URL
-    if [ -z "$ASSET_URL" ]; then
-        echo "Error: Couldn't find the asset URL for $ASSET_NAME in $REPO."
-        continue
-    fi
+  echo "Converted asset name to $ENCODED_ASSET_NAME."
 
-    # Download the asset
-    curl -L -o "$ASSET_NAME" "$ASSET_URL"
+  # Fetch the latest release from the GitHub API
+  API_URL="https://api.github.com/repos/$REPO/releases/latest"
+  echo "Fetching asset url from $API_URL..."
 
-    # Verify if the file was downloaded
-    if [ -f "$ASSET_NAME" ]; then
-        echo "$ASSET_NAME downloaded successfully from $REPO, installing."
-        mkdir -p /usr/share/gnome-shell/extensions/tmp
-        unzip "$ASSET_NAME" -d /usr/share/gnome-shell/extensions/tmp
-        ASSET_UUID=$(jq -r '.uuid' /usr/share/gnome-shell/extensions/tmp/metadata.json)
-        mv /usr/share/gnome-shell/extensions/tmp "/usr/share/gnome-shell/extensions/$ASSET_UUID"
-        rm "$ASSET_NAME"
+  ASSET_URL=$(curl -s $API_URL | grep "browser_download_url" | grep "$ENCODED_ASSET_NAME" | grep -v ".asc" | cut -d '"' -f 4)
+  echo "Obtained asset url $ASSET_URL."
+
+  # Check if we got the asset URL
+  if [ -z "$ASSET_URL" ]; then
+    echo "Error: Couldn't find the asset URL for $ASSET_NAME in $REPO."
+    continue
+  fi
+
+  # Download the asset
+  curl -L -o "$ASSET_NAME" "$ASSET_URL"
+
+  # Verify if the file was downloaded
+  if [ -f "$ASSET_NAME" ]; then
+    echo "$ASSET_NAME downloaded successfully from $REPO, installing."
+    mkdir -p /usr/share/gnome-shell/extensions/tmp
+    unzip "$ASSET_NAME" -d /usr/share/gnome-shell/extensions/tmp
+    ASSET_UUID=$(jq -r '.uuid' /usr/share/gnome-shell/extensions/tmp/metadata.json)
+    mv /usr/share/gnome-shell/extensions/tmp "/usr/share/gnome-shell/extensions/$ASSET_UUID"
+    rm "$ASSET_NAME"
+    cd "/usr/share/gnome-shell/extensions/$ASSET_UUID"
+    glib-compile-schemas .
+    if [ -f "schemas/gschemas.compiled"]; then
+      echo "Extension $ASSET_NAME successfully installed."
     else
-        echo "Error: Failed to download $ASSET_NAME from $REPO."
+      echo "Error installing $ASSET_NAME."
     fi
+  else
+    echo "Error: Failed to download $ASSET_NAME from $REPO."
+  fi
 done
