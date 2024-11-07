34b235f5b93041f680bea5ad70b9ae3e5bd7a6e3
rommeswi <rommeswi@protonmail.com>
2024-11-07 21:58:24 +0900

    Update and rename nerdfont-install.sh to nerdfont-install.sh
    adds two fonts

--- includes.container/nerdfonts/nerdfont-install.sh
+++ includes.container/nerdfonts/nerdfont-install.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
-mkdir -p /fonts/SpaceMono
-cd /fonts/SpaceMono
+mkdir -p /nerdfonts/SpaceMono
+cd /nerdfonts/SpaceMono
 FONT_URL=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*SpaceMono.tar.xz' | cut -d '"' -f 4)
 curl $FONT_URL -fLo ./SpaceMono.tar.xz
 tar -xf SpaceMono.tar.xz
 rm SpaceMono.tar.xz
-mkdir -p /fonts/0xProto
-cd /fonts/0xProto
+mkdir -p /nerdfonts/0xProto
+cd /nerdfonts/0xProto
 FONT_URL=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*0xProto.tar.xz' | cut -d '"' -f 4)
 curl $FONT_URL -fLo ./0xProto.tar.xz
 tar -xf 0xProto.tar.xz
 rm 0xProto.tar.xz
-mkdir -p /fonts/Meslo
-cd /fonts/Meslo
+mkdir -p /nerdfonts/Meslo
+cd /nerdfonts/Meslo
 FONT_URL=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*Meslo.tar.xz' | cut -d '"' -f 4)
 curl $FONT_URL -fLo ./Meslo.tar.xz
 tar -xf Meslo.tar.xz
 rm Meslo.tar.xz
-mkdir -p /fonts/SauceCodePro
-cd /fonts/SauceCodePro
+mkdir -p /nerdfonts/SauceCodePro
+cd /nerdfonts/SauceCodePro
 FONT_URL=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*SourceCodePro.tar.xz' | cut -d '"' -f 4)
 curl $FONT_URL -fLo ./SourceCodePro.tar.xz
 tar -xf SourceCodePro.tar.xz
 rm SourceCodePro.tar.xz
-mv -f /fonts/* /usr/local/share/fonts/
+mkdir -p /nerdfonts/MPlus
+cd /nerdfonts/MPlus
+FONT_URL=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*MPlus.tar.xz' | cut -d '"' -f 4)
+curl $FONT_URL -fLo ./MPlus.tar.xz
+tar -xf MPlus.tar.xz
+rm MPlus.tar.xz
+mkdir -p /nerdfonts/HeavyData
+cd /nerdfonts/HeavyData
+FONT_URL=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*HeavyData.tar.xz' | cut -d '"' -f 4)
+curl $FONT_URL -fLo ./HeavyData.tar.xz
+tar -xf HeavyData.tar.xz
+rm HeavyData.tar.xz
+mv -f /nerdfonts/* /usr/local/share/fonts/
 fc-cache -f -v
